09aa91f47dbd4124e208eb0758c1bf237042ddc5
Caleb Post <postcn@rose-hulman.edu>
2013-03-18 22:17:42 -0400

    Believe Deck is complete and tested.
    All functionality I believe that deck needs is implemented.

--- Dominion/Dominion/Deck.cs
+++ Dominion/Dominion/Deck.cs
@@ -41,6 +41,10 @@ namespace Dominion
 
         public Card draw()
         {
+            if (inDeck.Count <= 0)
+            {
+                this.reshuffle();
+            }
             if (inDeck.Count > 0)
             {
                 Card drawn = inDeck[0];
@@ -48,6 +52,48 @@ namespace Dominion
                 return drawn;
             }
             return null;
+        }
+
+        public void discard(Card c)
+        {
+            this.inDiscard.Add(c);
+        }
+
+        public static void Shuffle<T>(List<T> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        /**
+         * This will get a list of the cards currently in the deck
+         */
+        public List<Card> getInDeck()
+        {
+            return this.inDeck;
+        }
+
+        public List<Card> getInDiscard()
+        {
+            return this.inDiscard;
+        }
+
+        /**
+         * Adds the discard back into the deck and shuffles it.
+         */ 
+        public void reshuffle()
+        {
+            this.inDeck.AddRange(this.inDiscard);
+            this.inDiscard.Clear();
+            Shuffle<Card>(this.inDeck);
         }
     }
 }
--- Dominion/Dominion/DeckTest.cs
+++ Dominion/Dominion/DeckTest.cs
@@ -37,6 +37,7 @@ namespace Dominion
             Assert.NotNull(drawn);
             Assert.AreEqual(9, d.cardsLeft());
         }
+
         [Test()]
         public void testDrawAll()
         {
@@ -46,5 +47,69 @@ namespace Dominion
             }
             Assert.AreEqual(0, d.cardsLeft());
         }
+
+        //This test works with randomness so it may fail
+        //sometimes without actually being a failure.
+        //This is just to ensure that we get sufficient differences
+        [Test()]
+        public void testShuffle()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                String nonShuffled = getString(d.getInDeck());
+                Deck.Shuffle<Card>(d.getInDeck());
+                Assert.AreNotEqual(nonShuffled, getString(d.getInDeck()));
+            }
+        }
+
+        //Used only with the above test to make sure the shuffle is working
+        private String getString(List<Card> c)
+        {
+            String val = "";
+            foreach (Card ca in c)
+            {
+                val += ca.getType() + ca.getCash();
+            }
+            return val;
+        }
+
+        [Test()]
+        public void testDiscardAll()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Card c = d.draw();
+                d.discard(c);
+            }
+            Assert.AreEqual(10, d.getInDiscard().Count);
+        }
+
+        [Test()]
+        public void testDiscardRandom()
+        {
+            Random rg = new Random();
+            int num = rg.Next(d.cardsLeft());
+            for (int i = 0; i < num; i++)
+            {
+                Card c = d.draw();
+                d.discard(c);
+            }
+            Assert.AreEqual(num, d.getInDiscard().Count);
+        }
+
+        [Test()]
+        public void testDrawAndDiscardAllAutomaticShuffle()
+        {
+            int size = d.cardsLeft();
+            for (int i = 0; i < size; i++)
+            {
+                Card c = d.draw();
+                d.discard(c);
+            }
+            //this draw should reshuffle the deck.
+            d.draw();
+            Assert.AreEqual(size-1, d.cardsLeft());
+        }
+            
     }
 }
