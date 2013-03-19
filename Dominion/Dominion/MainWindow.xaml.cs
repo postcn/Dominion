6cc2f2659ca225f513b9f23cc618e216e6ffd4aa
mathbum <mathbum112358@yahoo.com>
2013-03-19 14:55:24 -0400

    Images

--- Dominion/Dominion/MainWindow.xaml.cs
+++ Dominion/Dominion/MainWindow.xaml.cs
@@ -18,8 +18,7 @@ namespace Dominion
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
+    public partial class MainWindow : Window{
         public MainWindow(){
             InitializeComponent();
         }
