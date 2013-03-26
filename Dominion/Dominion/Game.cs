bb1dce1c8b04b650c0c2ef1819fab3cb55a27dfb
Caleb Post <postcn@rose-hulman.edu>
2013-03-26 12:53:51 -0400

    Added more tests
    Also messed with the player to add an id for testing purposes.

--- Dominion/Dominion/Game.cs
+++ Dominion/Dominion/Game.cs
@@ -18,7 +18,7 @@ namespace Dominion
             players = new List<Player>();
             for (int i = 0; i < numPlayers; i++)
             {
-                players.Add(new Player());
+                players.Add(new Player(i));
             }
             this.currentPlayer = 0;
             this.numPlayers = numPlayers;
@@ -51,15 +51,17 @@ namespace Dominion
         {
             //Instantiate must have cards;
 
-            //Instantiate Copper
+            //Instantiate Currency
             this.buyables = new List<CardStack>();
             this.buyables.Add(new CardStack(120, new Card(1, 1, 0, 0, 0, 0, 0, "Copper", "1 Currency Value", 0)));
             this.buyables.Add(new CardStack(120, new Card(1, 2, 0, 0, 0, 0, 0, "Silver", "2 Currency Value", 3)));
             this.buyables.Add(new CardStack(120, new Card(1, 3, 0, 0, 0, 0, 0, "Gold", "3 Currency Value", 6)));
+            //Instantiate victory cards
             int numInStack = this.calculateSupplyForVictory();
             this.buyables.Add(new CardStack(numInStack, new Card(0, 0, 0, 0, 1, 0, 0, "Estate", "1 Victory Point", 2)));
             this.buyables.Add(new CardStack(numInStack, new Card(0, 0, 0, 0, 3, 0, 0, "Duchy", "3 Victory Points", 5)));
             this.buyables.Add(new CardStack(numInStack, new Card(0, 0, 0, 0, 6, 0, 0, "Province", "6 Victory Points", 8)));
+            //TODO: get cards out of possible list.
         }
 
         public int calculateSupplyForVictory()
