654dde228a5fe6e3296a48722cb1f17888e9b9cb
mathbum <mathbum112358@yahoo.com>
2013-04-13 22:35:00 -0400

    actions

--- Dominion/Dominion/MainWindow.xaml.cs
+++ Dominion/Dominion/MainWindow.xaml.cs
@@ -33,33 +33,227 @@ namespace Dominion{
         List<string> playerNames;
         List<CardStack> stacks;
         string currentCard,lastCard, handCard;
-        List<Image> victoryImage, currencyImage,handImage;
-        List<Button> currencyButton,victoryButton,handButton;
+        List<Image> victoryImage, currencyImage,handImage,actionImage;
+        List<Button> currencyButton,victoryButton,handButton,actionButton;
 
+        private void Confirm_Click(object sender, RoutedEventArgs e) {
+
+        }
+        private void PlayerCont() {
+            //player.cleanUp();
+            ResetCards();
+            currentCard = "";
+
+           // player.getHand().draw(player.getDeck());
+            int length=player.getHand().size();
+            int panelsize = 400 + (length-5) * 80;
+            stackpan.Width = panelsize;
+            Hand myHand = player.getHand();
+            for (int i = 0; i < length; i++) {
+                string name = myHand.getHand()[i].toString() + ".jpg";
+                SetPicture(name, handImage[i]);
+            }
+            player.getCurrency();
+        }
+    
+        private void Cleanup_Click(object sender, RoutedEventArgs e){
+            this.Hide();
+            playernum = myGame.nextTurn();
+            PrepScreen prep = new PrepScreen(playerNames[playernum],this);
+            prep.Show();
+            player = myGame.getCurrentPlayer();
+            player.cleanUp();
+            PlayerCont();
+            //switch to next player
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e){
+            SetPicture("blank.jpg", Hand_Card);
+            CardDrawnLabel.Content = "Card Bought";
+            Description.Content = "Labez";
+            DescriptionLabel.Content = "Nothing Yet";
+        }
+
+        private void CurrencyImage_Click(object sender, RoutedEventArgs e) {
+            Image_Click(sender, currencyButton, currencyImage, false);
+        }
+        private void VictoryImage_Click(object sender, RoutedEventArgs e) {
+            Image_Click(sender, victoryButton, victoryImage, false);
+        }
+        private void HandImage_Click(object sender, RoutedEventArgs e) {
+            Image_Click(sender, handButton, handImage, true);   
+        }
+        private void ActionImage_Click(object sender, RoutedEventArgs e) {
+            Image_Click(sender, actionButton, actionImage, false);
+        }
+        private void Image_Click(object sender,List<Button> buttons,List<Image> images,Boolean handcard) {
+            Button obj = (Button) sender;
+            for (int i = 0; i < buttons.Count(); i++) {
+                if (buttons[i] == obj) {
+                    currentCard = StripImageSource(images[i].Source.ToString());
+                    HilightCard(images[i], handcard);
+                }
+            }
+        }
+    /*private void HandImage1_Click(object sender, RoutedEventArgs e){
+            currentCard = stripImageSource(HandImage1.Source.ToString());
+            handCard = stripImageSource(HandImage1.Source.ToString());
+            hilightCard(HandImage1, true);
+            Buy.IsEnabled = false;
+        }*/
+
+        private string StripImageSource(string str){
+            int length = str.Count();   
+            return str.Substring(42, length - 46);
+        }
+        private void Buy_Click(object sender, RoutedEventArgs e){
+            if (currentCard.Equals("") || handCard.Equals(currentCard)){
+                Description.Content = "A Buyable Card isn't Selected";
+                ResetCards();
+                return;
+            }
+            Boolean work = false;
+            int length = stacks.Count();
+            for (int i = 0; i < length; i++){
+                if (stacks[i].getCard().toString().Equals(currentCard)){
+                    work = player.buy(stacks[i]);
+                }
+            }
+
+            if (!work){
+                Description.Content = "buy failed " + player.getCurrencyValue();
+            }else{
+                Description.Content = "Buy Sucessful";
+                string name = currentCard + ".jpg";
+                SetPicture(name, Hand_Card);
+            }
+            ResetCards();
+        }
+
+       
+        private void HilightCard(Image pic, Boolean isHandCard){
+            ResetCards();
+            string card;
+            Buy.IsEnabled = false;
+            if (!currentCard.Contains("1")){
+                card = currentCard + "1" + ".jpg";
+                if (!isHandCard){
+                    Buy.IsEnabled = true;
+                }
+            }else{
+                card = currentCard.Substring(0, currentCard.Count() - 1) + ".jpg";
+            }
+
+            lastCard = card.Substring(0, card.Count() - 4);
+            SetPicture(card, pic);
+
+        }
+       
+        private void SetPicture(string str, Image pic) {
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(str, UriKind.RelativeOrAbsolute);
+            image.EndInit();
+            pic.Source = image;
+        }
+        private void ResetCards() {
+            if (lastCard != "") {
+                Buy.IsEnabled = false;
+                string card = lastCard;
+                if (lastCard.Contains("1")) {
+                    card = lastCard.Substring(0, lastCard.Count() - 1);
+                }
+                card = card + ".jpg";
+                int i;
+                for (i = 0; i < currencyImage.Count(); i++) {
+                    if (StripImageSource(currencyImage[i].Source.ToString()).Equals(lastCard)) {
+                        SetPicture(card, currencyImage[i]);
+                        return;
+                    }
+                }
+                for (i = 0; i < handImage.Count(); i++) {
+                    if (StripImageSource(handImage[i].Source.ToString()).Equals(lastCard)) {
+                        SetPicture(card, handImage[i]);
+                        return;
+                    }
+                }
+                for (i = 0; i < victoryImage.Count(); i++) {
+                    if (victoryImage[i].IsEnabled == true) {
+                        if (StripImageSource(victoryImage[i].Source.ToString()).Equals(lastCard)) {
+                            SetPicture(card, victoryImage[i]);
+                            return;
+                        }
+                    }
+                }
+                for (i = 0; i < actionImage.Count(); i++) {
+                    if (StripImageSource(actionImage[i].Source.ToString()).Equals(lastCard)) {
+                        SetPicture(card, actionImage[i]);
+                        return;
+                    }
+                }
+            }
+        }
         /*********************************************************
          add playerinit and change start_click to initializegame
          ********************************************************/
         private void Initialize() {
             myGame = new Game(totalPlayers);
-//            myGame = new Game(1);
+            //            myGame = new Game(1);
             player = myGame.getCurrentPlayer();
             stacks = myGame.getBuyables();
             currentCard = "";
             lastCard = "";
             handCard = "";
             playernum = 0;
+            InitializeButtonImages();
 
+            //Confirm.IsEnabled = true;
+            //   List<Image> victory = new List<Image>();
+            //   List<Image> currency = new List<Image>();
+            // List<Image> action = new List<Image>();
+            //   List<Button> victoryButton = new List<Button>();
+            //   List<Button> currencyButton = new List<Button>();
+            //   List<Button> actionButton = new List<Button>();
+            int size = stacks.Count();
+            int i, victorys = 0, currencies = 0, actions = 0;
+            for (i = 0; i < size; i++) {
+                string name = stacks[i].getCard().getName() + ".jpg";
+                if (stacks[i].getCard().getType() == 0) {
+                    SetPicture(name, victoryImage[victorys]);
+                    victoryButton[victorys].IsEnabled = true;
+                    victorys++;
+                } else if (stacks[i].getCard().getType() == 1) {
+                    SetPicture(name, currencyImage[currencies]);
+                    currencyButton[currencies].IsEnabled = true;
+                    currencies++;
+                } else {
+                    SetPicture(name, actionImage[actions]);
+                    actionButton[actions].IsEnabled = true;
+                    actions++;
+                }
+            }
+
+            int handsize = 5;
+            Hand myHand = player.getHand();
+            for (i = 0; i < handsize; i++) {
+                string name = myHand.getHand()[i].toString() + ".jpg";
+                SetPicture(name, handImage[i]);
+            }
+            //
+            ActionButton1.IsEnabled = true;
+            SetPicture("Adventurer.jpg", actionImage[0]);
+            //
+            player.getCurrency();
+        }
+        private void InitializeButtonImages() {
             victoryButton = new List<Button>();
             currencyButton = new List<Button>();
+            actionButton = new List<Button>();
             currencyImage = new List<Image>();
             victoryImage = new List<Image>();
-            //Confirm.IsEnabled = true;
-         //   List<Image> victory = new List<Image>();
-         //   List<Image> currency = new List<Image>();
-            // List<Image> action = new List<Image>();
-         //   List<Button> victoryButton = new List<Button>();
-         //   List<Button> currencyButton = new List<Button>();
-            //   List<Button> actionButton = new List<Button>();
+            actionImage = new List<Image>();
+            handImage = new List<Image>();
+            handButton = new List<Button>();
             victoryImage.Add(VictoryImage1);
             victoryImage.Add(VictoryImage2);
             victoryImage.Add(VictoryImage3);
@@ -74,29 +268,26 @@ namespace Dominion{
             currencyButton.Add(CurrencyButton1);
             currencyButton.Add(CurrencyButton2);
             currencyButton.Add(CurrencyButton3);
-            int size = stacks.Count();
-            int i, victorys = 0, currencies = 0, actions = 0;
-            for (i = 0; i < size; i++) {
-                string name = stacks[i].getCard().getName() + ".jpg";
-                if (stacks[i].getCard().getType() == 0) {
-                    setPicture(name, victoryImage[victorys]);
-                    victoryImage[victorys].IsEnabled = true;
-                    victoryButton[victorys].IsEnabled = true;
-                    victorys++;
-                } else if (stacks[i].getCard().getType() == 1) {
-                    setPicture(name, currencyImage[currencies]);
-                    currencyImage[currencies].IsEnabled = true;
-                    currencyButton[currencies].IsEnabled = true;
-                    currencies++;
-                } else {
-                    //     action[actions].Source = image;
-                    //    action[actions].IsEnabled = true;
-                    //    actions++;
-                }
-            }
-            handImage = new List<Image>();
-            handButton = new List<Button>();
-            int length = 5;
+            actionImage.Add(ActionImage1);
+            actionImage.Add(ActionImage2);
+            actionImage.Add(ActionImage3);
+            actionImage.Add(ActionImage4);
+            actionImage.Add(ActionImage5);
+            actionImage.Add(ActionImage6);
+            actionImage.Add(ActionImage7);
+            actionImage.Add(ActionImage8);
+            actionImage.Add(ActionImage9);
+            actionImage.Add(ActionImage10);
+            actionButton.Add(ActionButton1);
+            actionButton.Add(ActionButton2);
+            actionButton.Add(ActionButton3);
+            actionButton.Add(ActionButton4);
+            actionButton.Add(ActionButton5);
+            actionButton.Add(ActionButton6);
+            actionButton.Add(ActionButton7);
+            actionButton.Add(ActionButton8);
+            actionButton.Add(ActionButton9);
+            actionButton.Add(ActionButton10);
             handImage.Add(HandImage1);
             handImage.Add(HandImage2);
             handImage.Add(HandImage3);
@@ -219,176 +410,6 @@ namespace Dominion{
             handButton.Add(HandButton48);
             handButton.Add(HandButton49);
             handButton.Add(HandButton50);
-            Hand myHand = player.getHand();
-            for (i = 0; i < length; i++) {
-                string name = myHand.getHand()[i].toString() + ".jpg";
-                setPicture(name, handImage[i]);
-            }
-            player.getCurrency();
-        }
-        private void playerCont() {
-            //player.cleanUp();
-            resetCards();
-            currentCard = "";
-
-           // player.getHand().draw(player.getDeck());
-            int length=player.getHand().size();
-            int panelsize = 400 + (length-5) * 80;
-            stackpan.Width = panelsize;
-            Hand myHand = player.getHand();
-            for (int i = 0; i < length; i++) {
-                string name = myHand.getHand()[i].toString() + ".jpg";
-                setPicture(name, handImage[i]);
-            }
-            player.getCurrency();
-        }
-    
-        private void Cleanup_Click(object sender, RoutedEventArgs e){
-            this.Hide();
-            playernum = myGame.nextTurn();
-            PrepScreen prep = new PrepScreen(playerNames[playernum],this);
-            prep.Show();
-            player = myGame.getCurrentPlayer();
-            player.cleanUp();
-            playerCont();
-            //switch to next player
-        }
-
-        private void Reset_Click(object sender, RoutedEventArgs e){
-            setPicture("blank.jpg", Hand_Card);
-            CardDrawnLabel.Content = "Card Bought";
-            Description.Content = "Labez";
-            DescriptionLabel.Content = "Nothing Yet";
-        }
-
-        private void CurrencyImage_Click(object sender, RoutedEventArgs e) {
-            Image_Click(sender, currencyButton, currencyImage, false);
-        }
-        private void VictoryImage_Click(object sender, RoutedEventArgs e) {
-            Image_Click(sender, victoryButton, victoryImage, false);
-        }
-        private void HandImage_Click(object sender, RoutedEventArgs e) {
-            Image_Click(sender, handButton, handImage, true);   
-        }
-        private void Image_Click(object sender,List<Button> buttons,List<Image> images,Boolean handcard) {
-            Button obj = (Button) sender;
-            for (int i = 0; i < buttons.Count(); i++) {
-                if (buttons[i] == obj) {
-                    currentCard = stripImageSource(images[i].Source.ToString());
-                    hilightCard(images[i], handcard);
-                }
-            }
-        }
-    /*private void HandImage1_Click(object sender, RoutedEventArgs e){
-            currentCard = stripImageSource(HandImage1.Source.ToString());
-            handCard = stripImageSource(HandImage1.Source.ToString());
-            hilightCard(HandImage1, true);
-            Buy.IsEnabled = false;
-        }*/
-
-        private string stripImageSource(string str){
-            int length = str.Count();   
-            return str.Substring(42, length - 46);
-        }
-        private void Buy_Click(object sender, RoutedEventArgs e){
-            if (currentCard.Equals("") || handCard.Equals(currentCard)){
-                Description.Content = "A Buyable Card isn't Selected";
-                resetCards();
-                return;
-            }
-            Boolean work = false;
-            int length = stacks.Count();
-            for (int i = 0; i < length; i++){
-                if (stacks[i].getCard().toString().Equals(currentCard)){
-                    work = player.buy(stacks[i]);
-                }
-            }
-
-            if (!work){
-                Description.Content = "buy failed " + player.getCurrencyValue();
-            }else{
-                Description.Content = "Buy Sucessful";
-                string name = currentCard + ".jpg";
-                setPicture(name, Hand_Card);
-            }
-            resetCards();
-        }
-
-       
-        private void hilightCard(Image pic, Boolean isHandCard){
-            resetCards();
-            string card;
-            Buy.IsEnabled = false;
-            if (!currentCard.Contains("1")){
-                card = currentCard + "1" + ".jpg";
-                if (!isHandCard){
-                    Buy.IsEnabled = true;
-                }
-            }else{
-                card = currentCard.Substring(0, currentCard.Count() - 1) + ".jpg";
-            }
-
-            lastCard = card.Substring(0, card.Count() - 4);
-            setPicture(card, pic);
-
-        }
-        private void resetCards(){
-            if (lastCard != ""){
-                Buy.IsEnabled = false;
-                string card = lastCard;
-                if (lastCard.Contains("1")){
-                    card = lastCard.Substring(0, lastCard.Count() - 1);
-                }
-                card = card + ".jpg";
-                List<Image> victory = new List<Image>();
-                List<Image> currency = new List<Image>();
-                List<Image> handImage = new List<Image>();
-                // List<Image> action = new List<Image>();
-                victory.Add(VictoryImage1);
-                victory.Add(VictoryImage2);
-                victory.Add(VictoryImage3);
-                victory.Add(VictoryImage4);
-                currency.Add(CurrencyImage1);
-                currency.Add(CurrencyImage2);
-                currency.Add(CurrencyImage3);
-                handImage.Add(HandImage1);
-                handImage.Add(HandImage2);
-                handImage.Add(HandImage3);
-                handImage.Add(HandImage4);
-                handImage.Add(HandImage5);
-                int i;
-                for (i = 0; i < currency.Count(); i++){
-                    if (stripImageSource(currency[i].Source.ToString()).Equals(lastCard)){
-                        setPicture(card, currency[i]);
-                        return;
-                    }
-                }
-                for (i = 0; i < handImage.Count(); i++){
-                    if (stripImageSource(handImage[i].Source.ToString()).Equals(lastCard)){
-                        setPicture(card, handImage[i]);
-                        return;
-                    }
-                }
-                for (i = 0; i < victory.Count(); i++){
-                    if (victory[i].IsEnabled == true){
-                        if (stripImageSource(victory[i].Source.ToString()).Equals(lastCard)){
-                            setPicture(card, victory[i]);
-                            return;
-                        }
-                    }
-                }
-            }
-        }
-        private void setPicture(string str, Image pic){
-            BitmapImage image = new BitmapImage();
-            image.BeginInit();
-            image.UriSource = new Uri(str, UriKind.RelativeOrAbsolute);
-            image.EndInit();
-            pic.Source = image;
-        }
-
-        private void Confirm_Click(object sender, RoutedEventArgs e){
-
         }
         //doens't check buys
         //shuffle cards well (like from start)
