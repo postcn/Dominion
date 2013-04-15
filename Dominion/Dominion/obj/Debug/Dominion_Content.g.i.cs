7f6e216f79fdaa3b13bc16ea3eb2a55c0e8fd29a
mathbum <mathbum112358@yahoo.com>
2013-04-15 09:45:52 -0400

    pics

--- Dominion/Dominion/obj/Debug/Dominion_Content.g.i.cs
+++ Dominion/Dominion/obj/Debug/Dominion_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("copy%20(2)%20of%20adventurer1.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("copy%20of%20copy%20of%20curse.jpg")]
 
 
